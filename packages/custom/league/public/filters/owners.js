3ddd13ea4daefa6cbbc901d166f507542e4a8057
Javier Muhrer <jmuhrer@hubspot.com>
2015-01-24 21:34:01 -0500

    standings tweak, mobilization, de-themed, fixed login

---
+++ packages/custom/league/public/filters/owners.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('mean.league').filter('owner', [function() {
+  return function(input) {
+    switch(input) {
+      case 'Geoff Brown':
+        return 'Gian';
+      case 'Javi Muhrer':
+        return 'Javi';
+      case 'Alex Zorn':
+        return 'Zorn';
+      case 'Jordan Rand':
+        return 'JBland';
+      default:
+        return 'Who dis?';
+    }
+  };
+}]);
